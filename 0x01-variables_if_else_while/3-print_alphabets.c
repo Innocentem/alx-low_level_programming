f6e26ed2cd9e434f9cf83e4496ee0cfee5efbce1
Innocentem <mwbzinno@gmail.com>
2023-08-13 17:27:22 -0700

    and then in uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always zero
+ */
+
+int main(void)
+
+{
+
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+putchar(1);
+
+for (i = 'A'; i <= 'Z'; i++)
+putchar(i);
+putchar('\n');
+
+return(0);
+
+}
