3ba79295cc523fd0f390a268e6795d0066f7f662
Innocentem <mwbzinno@gmail.com>
2023-08-13 17:47:13 -0700

    and then uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,14 +10,13 @@ int main(void)
 {
 
 char i;
-
 for (i = 'a'; i <= 'z'; i++)
-putchar(1);
+putchar(i);
 
 for (i = 'A'; i <= 'Z'; i++)
 putchar(i);
 putchar('\n');
 
-return(0);
+return (0);
 
 }
