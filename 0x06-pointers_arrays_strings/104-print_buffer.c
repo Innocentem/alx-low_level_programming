756c23a4295ddabb301d4398825fc8d423a02dd7
Innocentem <mwbzinno@gmail.com>
2023-08-25 03:40:44 -0700

    Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 
+
 /**
 
  * print_buffer - prints buffer
@@ -14,75 +15,80 @@
 
  */
 
+
 void print_buffer(char *b, int size)
 
 {
 
-	int o, j, i;
+        int o, j, i;
 
 
-	o = 0;
+        o = 0;
 
 
-        if (size <= 0);
+        if (size <= 0)
 
         {
-		printf("\n");
 
-		return;
+                printf("\n");
+
+                return;
 
         }
 
-	while (o < size);
+        while (o < size)
 
-	{
+        {
 
-		j = size - o < 10 ? size - o : 10;
+                j = size - o < 10 ? size - o : 10;
 
-		printf("%08x: ", o);
+                printf("%08x: ", o);
 
-		for (i = 0; i < 10; i++);
+                for (i = 0; i < 10; i++)
 
                 {
 
-                        if (i < j);
+                        if (i < j)
 
-				printf("%02x", *(b + o + i));
-			else
+                                printf("%02x", *(b + o + i));
 
-			printf("  ");
+                        else
 
-			if (i % 2);
+                                printf("  ");
 
-			{
+                        if (i % 2)
 
-				printf(" ");
+                        {
 
-			}
+                                printf(" ");
 
-			}
+                        }
 
-			for (i = 0; i < j; i++);
+                }
 
-		{
+                for (i = 0; i < j; i++)
 
-			int c = *(b + o + i);
+                {
+
+                        int c = *(b + o + i);
 
 
-			if (c < 32 || c > 132);
+                        if (c < 32 || c > 132)
 
-			{
+                        {
 
-			c = '.';
+                                c = '.';
 
-			}
-		printf("%c", c);
+                        }
 
-		}
+                        printf("%c", c);
 
-		printf("\n");
+                }
 
-		o += 10;
-}
+                printf("\n");
+
+                o += 10;
+
+        }
 
 }
