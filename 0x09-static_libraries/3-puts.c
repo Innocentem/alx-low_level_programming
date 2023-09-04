985bc5d655b71eaef671ebe800a917e4690f8f6d
Innocentem <mwbzinno@gmail.com>
2023-09-04 04:08:15 -0700

    the script

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,15 +1,19 @@
+#include <stdio.h>
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * _puts - prints the string
+ * @str: a string
+ *
+ * Return: Nothing
+ */
 
-int main(void)
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
 }
